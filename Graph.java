4252fdd25fdc87bcb97c02018a209f6d36eda8e0
Shreya Shristi <shristishreyac@gmail.com>
2025-10-11 21:43:13 +0530

    second commit

--- Graph.java
+++ Graph.java
@@ -1,19 +1,60 @@
-import java.util.*;
-public class Graph {
-    public static void main(String srgs[]){
-        int v=4;
+// import java.util.*;
+// public class Graph {
+//     public static void main(String srgs[]){
+//         int v=4;
 
-        ArrayList<Edge> graph[]=new ArrayList[v];
-    }
-}
-static class Edge{
-    int src;
-    int des;
-    public Edge(int s,int d){
-        this.src=s;
-        this.des=d; 
-    }
-}
-public static void cg(ArrayList<Edge> graph[]){
+//         ArrayList<Edge> graph[]=new ArrayList[v];
+//     }
+// }
+// static class Edge{
+//     int src;
+//     int des;
+//     public Edge(int s,int d){
+//         this.src=s;
+//         this.des=d; 
+//     }
+// }
+// public static void cg(ArrayList<Edge> graph[]){
     
+// }
+import java.util.*;
+public class Graph{
+    static class Edge{
+        int src;
+        int des;
+        public Edge(int s,int d){
+            this.src=s;
+            this.des=d;
+        }
+    }
+    public static void cg(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<Edge>();
+        }
+        //graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,2));
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,3));
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,3));
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,2));
+
+
+
+    }
+    public static void main(String args[]){
+        int v=4;
+        ArrayList<Edge> graph[]=new ArrayList[v];
+        cg(graph);
+        System.out.println("Adjacency List:");
+        for(int i=0;i<v;i++){
+            System.out.print(i+"->");
+            for(Edge e: graph[i]){
+                System.out.print(e.des+" ");
+            }
+            System.out.println();
+        }
+    }
+
 }
---
+++ Reverse.java
@@ -0,0 +1,33 @@
+//     public static void main(String args[]){
+//         Scanner s=new Scanner(System.in);
+//         System.out.print("Give:_");
+//         String str=s.nextLine();
+//         StringBuilder sb=new StringBuilder(str);
+//         for(int i=sb.length()-1;i>=0;i--){
+//             System.out.print(sb.charAt(i));
+//         }
+//     }
+// }
+import java.util.*;
+public class Reverse{
+    public static void  main(String args[]){
+        int[] a={23,0,1,-215,43,21,10};
+        //int min=a[0];
+        // for(int i=1;i<a.length;i++){
+        //     if(min>a[i]){
+        //         min=a[i];
+        //     }
+        // }
+        // System.out.print(min);
+        for(int i=0;i<a.length-1;i++){
+           int min=a[i];
+           for(int j=i+1;j<a.length;j++){
+            if(a[j]<min){
+                min=a[j];
+            a[i]=min;
+            }
+           }
+        }
+        System.out.print(Arrays.toString(a));
+    }
+}
